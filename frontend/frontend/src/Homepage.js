f0c3da6268612c75518307d9486144a3b8978d7f
ama-matioc <matiocamalia@gmail.com>
2024-06-08 19:01:27 +0300

    Add final change (minor)

--- frontend/frontend/src/Homepage.js
+++ frontend/frontend/src/Homepage.js
@@ -4,7 +4,23 @@ import Navbar from './Navbar'
 
 function Homepage() {
   return (
+    <div>
     <Navbar></Navbar>
+    <h1>Welcome to Cineflix - Your Ultimate Destination for Movie Enthusiasts!</h1>
+    <h3>At Cineflix, we believe in the magic of cinema. Step into a world where every frame tells a story, every character becomes a friend, and every journey leaves an indelible mark on your soul.
+
+Discover a vast collection of movies spanning across genres, eras, and cultures. Whether you're craving the adrenaline rush of an action-packed blockbuster, the heartwarming embrace of a timeless classic, or the edge-of-your-seat suspense of a gripping thriller, Cineflix has something for everyone.
+
+Immerse yourself in cinematic excellence as you explore our curated selection of films meticulously handpicked to cater to every taste and preference. From critically acclaimed masterpieces to hidden gems waiting to be unearthed, embark on a cinematic adventure like never before.
+
+Join our community of movie aficionados, where passion meets purpose and discussions flourish. Engage in lively debates, share your favorite movie moments, and connect with like-minded individuals who share your love for the silver screen.
+
+At Cineflix, the magic of movies comes alive. Whether you're a casual moviegoer, a seasoned cinephile, or someone simply looking for entertainment, let Cineflix be your trusted companion on your cinematic journey.
+
+Experience the wonder, the thrill, and the sheer joy of cinema at Cineflix - Where Every Frame Tells a Story.
+
+</h3>
+    </div>
   )
 }
 
